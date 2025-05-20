c5dc4e89c2f02e0e23fddf2f3ed5319b95e975cc
Rejen <rejenkiller@live.fr>
2025-05-20 17:13:53 +0200

    :feat: Added routes and protected routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,34 @@ import store from './stores/store.js'
 import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import ProtectedRoute from './routes/ProtectedRoute.jsx'
+import DashboardTemplate from './components/templates/DashboardTemplate.jsx'
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    index: true,
+    Component: App,
+  },
+  { 
+    path: "home",
+    Component: ProtectedRoute,
+    children: [
+      { index: true, Component: DashboardTemplate },
+    ]
+  },
+]);
+
+const root = document.getElementById('root');
+
+createRoot(root).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 )
--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -3,7 +3,8 @@ import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+    // const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+    const isAuthenticated = true; // Replace with actual authentication logic
 
     if (!isAuthenticated) {
         return <Navigate to="/" replace />;
